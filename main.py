822395ac0878c5824f7aad48eb9dd98aa20f6551
Roniika <106553920+Roniika@users.noreply.github.com>
2022-09-05 21:01:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from flask import request
+from flask import request, jsonify
 from config import app
 from service import *
 
@@ -7,147 +7,70 @@ from service import *
 @app.route("/users", methods=['GET', 'POST'])
 def get_users():
     if request.method == 'GET':
-        return app.response_class(
-            response=json.dumps(get_all_users(), indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return jsonify(get_all_users())
     elif request.method == 'POST':
-        if isinstance(request.json, list):
-            insert_data_user()
-        elif isinstance(request.json, dict):
-            insert_data_user([request.json])
-        else:
-            print("Непонятный тип данных")
+        new_item = insert_data(request.json, User)
 
-        return app.response_class(
-            response=json.dumps(request.json, indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return jsonify(new_item.to_dict())
 
 
 @app.route("/users/<int:user_id>", methods=['GET', 'PUT', 'DELETE'])
 def get_user_by_id(user_id):
     if request.method == 'GET':
         data = get_all_users_by_id(user_id)
-
-        return app.response_class(
-            response=json.dumps(data, indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-            )
+        return jsonify(data)
     elif request.method == 'PUT':
-        update_universal(User, user_id, request.json)
-        return app.response_class(
-            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        item = update_universal(User, user_id, request.json)
+        return jsonify(item.to_dict())
     elif request.method == 'DELETE':
         delete_universal(User, user_id)
-        return app.response_class(
-            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return '', 204
 
 
 @app.route("/orders", methods=['GET', 'POST'])
 def get_orders():
     if request.method == 'GET':
-        return app.response_class(
-            response=json.dumps(get_all_orders(), indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return jsonify(get_all_orders())
     elif request.method == 'POST':
-        if isinstance(request.json, list):
-            insert_data_order()
-        elif isinstance(request.json, dict):
-            insert_data_order([request.json])
-        else:
-            print("Непонятный тип данных")
+        new_item = insert_data(request.json, Order)
 
-        return app.response_class(
-            response=json.dumps(request.json, indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return jsonify(new_item.to_dict())
 
 
 @app.route("/orders/<int:order_id>", methods=['GET', 'PUT', 'DELETE'])
 def get_order_by_id(order_id):
     if request.method == 'GET':
         data = get_all_orders_by_id(order_id)
-        return app.response_class(
-            response=json.dumps(data, indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return jsonify(data)
     elif request.method == 'PUT':
-        update_universal(Order, order_id, request.json)
-        return app.response_class(
-            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        item = update_universal(Order, order_id, request.json)
+        return jsonify(item.to_dict())
     elif request.method == 'DELETE':
         delete_universal(Order, order_id)
-        return app.response_class(
-            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return '', 204
 
 
 @app.route("/offers", methods=['GET', 'POST'])
 def get_offers():
     if request.method == 'GET':
-        return app.response_class(
-            response=json.dumps(get_all_offers(), indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return jsonify(get_all_offers())
     elif request.method == 'POST':
-        if isinstance(request.json, list):
-            insert_data_offer()
-        elif isinstance(request.json, dict):
-            insert_data_offer([request.json])
-        else:
-            print("Непонятный тип данных")
+        new_item = insert_data(request.json, Offer)
 
-        return app.response_class(
-            response=json.dumps(request.json, indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return jsonify(new_item.to_dict())
 
 
 @app.route("/offers/<int:offer_id>", methods=['GET', 'PUT', 'DELETE'])
 def get_offer_by_id(offer_id):
     if request.method == 'GET':
         data = get_all_offers_by_id(offer_id)
-
-        return app.response_class(
-            response=json.dumps(data, indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-            )
+        return jsonify(data)
     elif request.method == 'PUT':
-        update_universal(Offer, offer_id, request.json)
-        return app.response_class(
-            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        item = update_universal(Offer, offer_id, request.json)
+        return jsonify(item.to_dict())
     elif request.method == 'DELETE':
         delete_universal(Offer, offer_id)
-        return app.response_class(
-            response=json.dumps(["OK"], indent=4, ensure_ascii=False),
-            status=200,
-            mimetype="application/json"
-        )
+        return '', 204
 
 
 if __name__ == '__main__':
